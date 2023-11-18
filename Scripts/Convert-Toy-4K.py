9d9fcccc0c906b6d3c2a7d71fbccb359c4451290
Kyle Lukaszek <kylelukaszek@gmail.com>
2023-11-17 21:24:29 -0500

    Added script to convert Toy-4K-point clouds into Open3D plys

---
+++ Scripts/Convert-Toy-4K.py
@@ -0,0 +1,49 @@
+import open3d as o3d
+import numpy as np
+
+# Example
+
+# Load npz file containing point cloud and normals
+data = np.load('/mnt/c/Classes/CIS4780/3D-Classification/Data/toys4k_point_clouds/chicken/chicken_000/pc10K.npz', allow_pickle=True)
+data = data['dct']
+data = data.item()
+
+# Convert point cloud and normals to list
+pc = data['pc']
+pc = pc.tolist()
+normal = data['normals']
+normal = normal.tolist()
+
+data = []
+
+# Combine point cloud and normals into one list [[x, y, z, nx, ny, nz], ...]
+for i in enumerate(pc):
+    l = []
+    l.extend(pc[i])
+    l.extend(normal[i])
+    data.append(l)
+
+# Save data if needed
+with open('chicken.xyzn', 'w') as f:
+    for item in data:
+        for vertex in item:
+           f.write("%s, " % vertex)
+        f.write("\n" % item)
+
+# Convert list to numpy array
+data = np.array(data)
+
+# Create open3d point cloud
+pcd = o3d.geometry.PointCloud()
+
+# Read first 3 columns of numpy array as point cloud
+pcd.points = o3d.utility.Vector3dVector(data[:, 0:3])
+
+# Read last 3 columns of numpy array as normals
+pcd.normals = o3d.utility.Vector3dVector(data[:, 3:6])
+
+# Paint point cloud black (Or use color if provided)
+pcd.paint_uniform_color([0, 0, 0])
+
+# Visualize point cloud to make sure it looks correct
+o3d.visualization.draw_geometries([pcd])
