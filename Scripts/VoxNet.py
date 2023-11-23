ff1bba6a07fcecca592b8f971f5f3682cc602153
ljanikjo <ljanikjo@uoguelph.ca>
2023-11-22 19:52:30 -0500

    VoxNet runs without errors, haven't done extended testing

--- Scripts/VoxNet.py
+++ Scripts/VoxNet.py
@@ -17,10 +17,12 @@ class VoxNet(nn.Module):
 
         self.num_classes = num_classes
 
-        self.conv_1 = nn.Conv3d(1, 32, kernel_size=5, stride=2)
+        self.conv_1 = nn.Conv3d(32, 32, kernel_size=5, stride=2)
         self.conv_2 = nn.Conv3d(32, 32, kernel_size=3, stride=1)
         self.pool = nn.MaxPool3d(2)
-        self.full = nn.Linear(____, 128)
+        # I believe the input size should be number of out_channels of last conv3d layer
+        # multiplied by the size of the data after pooling: (6,6,6)
+        self.full = nn.Linear((6*6*6), 128)
         self.out = nn.Linear(128, self.num_classes)
 
         self.relu = nn.ReLU()
@@ -34,9 +36,18 @@ class VoxNet(nn.Module):
         x = self.relu(x)
         
         x = self.pool(x)
-        
+        x = self.drop(x)
+
+        # Flatten input
+        x = x.view(x.size(0), -1)
+
+        # print(x.size())
+
         x = self.full(x)
+        
+        # x = self.full(x)
         x = self.relu(x)
+        x = self.drop(x)
         x = self.out(x)
 
         return x
